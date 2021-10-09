fa58764140eb71eca0d0c95783ea2ec487e28b65
lanly_dev <lanly-dev@users.noreply.github.com>
2021-10-08 22:34:33 -0700

    add pug

--- src/vite/components/HelloWorld.vue
+++ src/vite/components/HelloWorld.vue
@@ -1,4 +1,4 @@
-<script setup lang="ts">
+<script setup lang='ts'>
 import { ref } from 'vue'
 
 defineProps<{ msg: string }>()
@@ -6,31 +6,18 @@ defineProps<{ msg: string }>()
 const count = ref(0)
 </script>
 
-<template>
-  <h1>{{ msg }}</h1>
-
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
-
-  <p>See <code>README.md</code> for more information.</p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Docs
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
-  </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+<template lang='pug'>
+h1 {{ msg }}
+p.
+  Recommended IDE setup:
+  #[a(href='https://code.visualstudio.com/', target='_blank') VSCode] +
+  #[a(href='https://github.com/johnsoncodehk/volar', target='_blank') Volar]
+p See #[code README.md] for more information.
+p.
+  #[a(href='https://vitejs.dev/guide/features.html', target='_blank') Vite Docs] |
+  #[a(href='https://v3.vuejs.org/', target='_blank') Vue 3 Docs]
+button(type='button', @click='count++') count is: {{ count }}
+p Edit #[code components/HelloWorld.vue] to test hot module replacement.
 </template>
 
 <style scoped>
